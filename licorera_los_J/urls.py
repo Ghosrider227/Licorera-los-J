2adcfe56b17121386269b04b34b583af4978c021
brido-PGM <brido0002@gmail.com>
2025-04-07 08:18:32 -0500

    pequeños ajustes en urls

--- licorera_los_J/urls.py
+++ licorera_los_J/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     #Principales
@@ -18,4 +21,4 @@ urlpatterns = [
     path("eliminar_productos/<int:id_productos>/", views.eliminar_productos, name='eliminar_productos'),
     path("agregar_productos/", views.agregar_productos, name="agregar_productos"),
     path("editar_productos/<int:id_productos>/", views.editar_productos, name='editar_producto'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
